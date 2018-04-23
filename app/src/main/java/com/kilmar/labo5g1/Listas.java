dd829c273efec7ee541c45816b1dc797f70f28ad
CarMer194 <00060313@uca.edu.sv>
2018-04-22 19:27:05 -0600

    preparando para cambios a la clase lista

---
+++ app/src/main/java/com/kilmar/labo5g1/Listas.java
@@ -0,0 +1,47 @@
+package com.kilmar.labo5g1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Carlos on 22/4/2018.
+ */
+
+public class Listas {
+    List<Planeta> planetaList;
+    List<Planeta> listAUX;
+    boolean favorito;
+
+    public Listas() {
+        planetaList = fillList();
+        listAUX = fillList();
+    }
+
+    public ArrayList<Planeta> fillList(){
+        ArrayList<Planeta> l = new ArrayList<>();
+        String desc= " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae enim tristique, auctor nunc a, porta lorem. Aliquam egestas ullamcorper imperdiet. Praesent quis elit tristique, mollis est id, venenatis lectus. Cras eu maximus dolor, non tempor leo. Etiam cursus sapien sit amet eros cursus porttitor. Fusce diam nibh, tincidunt a lectus non, elementum viverra nisl. Nulla venenatis neque nec risus convallis maximus. Mauris vel enim eu mauris blandit maximus.\n" +
+                "\n" +
+                "Vivamus sed ex interdum, condimentum ligula sed, hendrerit tellus. Integer eu vehicula diam. Donec tempus nibh lectus, nec interdum risus suscipit in. Vestibulum dignissim imperdiet lorem, a maximus arcu maximus a. Quisque vel elementum ipsum. Phasellus rutrum ut elit pulvinar aliquam. Praesent sagittis elit a tortor bibendum, sodales pellentesque tortor sollicitudin. Nam posuere volutpat dolor vitae vehicula. Nulla commodo augue mi, vitae accumsan enim ullamcorper a. Integer vitae bibendum libero, ac fermentum eros. Vivamus consectetur orci nisi, rhoncus aliquet odio viverra sed. Maecenas efficitur rutrum aliquet. Vivamus sed risus blandit, pharetra justo placerat, cursus orci. Vivamus sit amet vestibulum diam. Vivamus vel fermentum neque. ";
+        l.add(new Planeta(1,"una pelicula",desc));
+        l.add(new Planeta(2,"otra pelicula",desc));
+        l.add(new Planeta(3,"otra otra pelicula",desc));
+
+        return l;
+    }
+
+    public List<Planeta> getPlanetaList() {
+        return planetaList;
+    }
+
+    public void setPlanetaList(List<Planeta> planetaList) {
+        this.planetaList = planetaList;
+    }
+
+    public List<Planeta> getListAUX() {
+        return listAUX;
+    }
+
+    public void setListAUX(List<Planeta> listAUX) {
+        this.listAUX = listAUX;
+    }
+}
--- app/src/main/java/com/kilmar/labo5g1/MainActivity.java
+++ app/src/main/java/com/kilmar/labo5g1/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     List<Planeta> planetaList;
+    List<Planeta> listAUX;
     RecyclerView recyclerView;
     PlanetAdapter adapter;
 
@@ -19,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         planetaList = fillList();
+        listAUX = fillList();
+
 
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
@@ -33,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private ArrayList<Planeta> fillList(){
+    public ArrayList<Planeta> fillList(){
         ArrayList<Planeta> l = new ArrayList<>();
         String desc= " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae enim tristique, auctor nunc a, porta lorem. Aliquam egestas ullamcorper imperdiet. Praesent quis elit tristique, mollis est id, venenatis lectus. Cras eu maximus dolor, non tempor leo. Etiam cursus sapien sit amet eros cursus porttitor. Fusce diam nibh, tincidunt a lectus non, elementum viverra nisl. Nulla venenatis neque nec risus convallis maximus. Mauris vel enim eu mauris blandit maximus.\n" +
                 "\n" +
@@ -47,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void cambiarVista1(View view){
-        //recyclerView.destroyDrawingCache();
-        planetaList = fillList();
+        planetaList = listAUX;
+        System.out.println("vista 1");
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
 
@@ -62,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
     public void cambiarVista2(View view){
         //recyclerView.destroyDrawingCache();
         planetaList = fillList2();
-
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
 
         adapter = new PlanetAdapter(this,planetaList);
@@ -72,12 +75,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private ArrayList<Planeta> fillList2(){
+    public ArrayList<Planeta> fillList2(){
         ArrayList<Planeta> l = (ArrayList<Planeta>) adapter.getPlanetaList();
+        listAUX= adapter.getPlanetaList();
+        System.out.println("prueba: "+listAUX.get(1).getIdPlaneta());
         int i=0;
-        String desc= " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae enim tristique, auctor nunc a, porta lorem. Aliquam egestas ullamcorper imperdiet. Praesent quis elit tristique, mollis est id, venenatis lectus. Cras eu maximus dolor, non tempor leo. Etiam cursus sapien sit amet eros cursus porttitor. Fusce diam nibh, tincidunt a lectus non, elementum viverra nisl. Nulla venenatis neque nec risus convallis maximus. Mauris vel enim eu mauris blandit maximus.\n" +
-                "\n" +
-                "Vivamus sed ex interdum, condimentum ligula sed, hendrerit tellus. Integer eu vehicula diam. Donec tempus nibh lectus, nec interdum risus suscipit in. Vestibulum dignissim imperdiet lorem, a maximus arcu maximus a. Quisque vel elementum ipsum. Phasellus rutrum ut elit pulvinar aliquam. Praesent sagittis elit a tortor bibendum, sodales pellentesque tortor sollicitudin. Nam posuere volutpat dolor vitae vehicula. Nulla commodo augue mi, vitae accumsan enim ullamcorper a. Integer vitae bibendum libero, ac fermentum eros. Vivamus consectetur orci nisi, rhoncus aliquet odio viverra sed. Maecenas efficitur rutrum aliquet. Vivamus sed risus blandit, pharetra justo placerat, cursus orci. Vivamus sit amet vestibulum diam. Vivamus vel fermentum neque. ";
         System.out.println("size es: "+l.size());
 
         for (i=0;i<l.size();i++){
